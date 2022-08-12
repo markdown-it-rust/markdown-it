cc621e2249c7c5a7cc3fec9930bae59d5f1576a7
Alex Kocharin <alex@kocharin.ru>
2022-08-12 12:17:04 +0300

    add more demo stuff

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -4,10 +4,11 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn main() {
+    console_error_panic_hook::set_once();
+
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
     let textarea = document.get_element_by_id("source").unwrap();
-    let output = document.get_element_by_id("target").unwrap();
 
     let mut parser = markdown_it::MarkdownIt::new();
     markdown_it::plugins::cmark::add(&mut parser);
@@ -19,8 +20,29 @@ pub fn main() {
     let timeout = timeout_.clone();
     let do_render = Closure::<dyn FnMut()>::new(move || {
         let src = textarea.dyn_ref::<web_sys::HtmlTextAreaElement>().unwrap().value();
-        let html = parser.parse(&src).render();
-        output.set_inner_html(&html);
+        let ast =  parser.parse(&src);
+        let html = ast.render();
+        let window = web_sys::window().unwrap();
+        let document = window.document().unwrap();
+        let preview = document.get_element_by_id("result-preview").expect("no #result-preview");
+        preview.set_inner_html(&html);
+        let result_html = document.get_element_by_id("result-html").expect("no #result-html");
+        result_html.set_text_content(Some(&html));
+
+        let mut ast_str = String::new();
+        ast.walk(|node, depth| {
+            ast_str.push_str(&"    ".repeat(depth as usize));
+            let name = &node.name()[node.name().rfind("::").map(|x| x+2).unwrap_or_default()..];
+            if let Some(data) = node.cast::<markdown_it::parser::inline::Text>() {
+                ast_str.push_str(&format!("{}: {:?}\n", name, data.content));
+            } else if let Some(data) = node.cast::<markdown_it::parser::inline::TextSpecial>() {
+                ast_str.push_str(&format!("{}: {:?}\n", name, data.content));
+            } else {
+                ast_str.push_str(&format!("{}\n", name));
+            }
+        });
+        let result_ast = document.get_element_by_id("result-ast").expect("no #result-ast");
+        result_ast.set_text_content(Some(&ast_str));
         timeout.set(None);
     });
 
@@ -29,7 +51,7 @@ pub fn main() {
 
     let timeout = timeout_;
     const DEBOUNCE_TIMEOUT : i32 = 100; // ms
-    let input_handler = Closure::<dyn FnMut(_)>::new(move |_: web_sys::InputEvent| {
+    let input_handler = Closure::<dyn FnMut(_)>::new(move |_: web_sys::Event| {
         let now = Date::now();
         if (now - last_exec).abs() < DEBOUNCE_TIMEOUT as f64 {
             if timeout.get().is_none() {
@@ -50,4 +72,35 @@ pub fn main() {
     let textarea = document.get_element_by_id("source").unwrap();
     textarea.add_event_listener_with_callback("input", input_handler.as_ref().unchecked_ref()).unwrap();
     input_handler.forget();
+
+    let tab_click = Closure::<dyn FnMut(_)>::new(move |event: web_sys::Event| {
+        let window = web_sys::window().unwrap();
+        let document = window.document().unwrap();
+        let tabs = document.query_selector_all("#tabs-toggle a.nav-link.active").unwrap();
+        for idx in 0..tabs.length() {
+            let tab = tabs.get(idx).unwrap();
+            let el = tab.dyn_into::<web_sys::HtmlElement>().unwrap();
+            el.class_list().remove_1("active").unwrap();
+        }
+        let el = event.target().unwrap().dyn_into::<web_sys::HtmlElement>().unwrap();
+        el.class_list().add_1("active").unwrap();
+
+        let contents = document.query_selector_all(".tab-content").unwrap();
+        for idx in 0..contents.length() {
+            let el = contents.get(idx).unwrap().dyn_into::<web_sys::HtmlElement>().unwrap();
+            el.style().set_property("display", "none").unwrap();
+        }
+
+        let toggle = el.dataset().get("toggle").expect("no data-toggle attribute on anchor");
+        let el = document.get_element_by_id(&toggle).unwrap().dyn_into::<web_sys::HtmlElement>().unwrap();
+        el.style().set_property("display", "block").unwrap();
+        event.prevent_default();
+    });
+
+    let tabs = document.query_selector_all("#tabs-toggle a.nav-link").unwrap();
+    for idx in 0..tabs.length() {
+        tabs.get(idx).unwrap().add_event_listener_with_callback("click", tab_click.as_ref().unchecked_ref()).unwrap();
+    }
+    tabs.get(0).unwrap().dispatch_event(&web_sys::Event::new("click").unwrap()).unwrap();
+    tab_click.forget();
 }
