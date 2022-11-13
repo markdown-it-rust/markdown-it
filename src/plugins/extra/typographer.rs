2862f831ba19247d093499c8cd6355567d01b622
Daan Wynen <black.puppydog@gmx.de>
2022-11-13 13:45:29 +0100

    Replace "+-" with "±"
    The `plus_minus` test works!
    The rest should be a walk in the park now. 😆

--- src/plugins/extra/typographer.rs
+++ src/plugins/extra/typographer.rs
@@ -3,6 +3,11 @@ use crate::parser::core::CoreRule;
 use crate::parser::inline::Text;
 use crate::{MarkdownIt, Node};
 
+use once_cell::sync::OnceCell;
+use regex::Regex;
+
+static REPLACEMENTS: OnceCell<Box<[(Regex, &'static str)]>> = OnceCell::new();
+
 pub fn add(md: &mut MarkdownIt) {
     md.add_rule::<TypographerRule>();
 }
@@ -11,7 +16,20 @@ pub struct TypographerRule;
 impl CoreRule for TypographerRule {
     fn run(root: &mut Node, _: &MarkdownIt) {
         root.walk_mut(|node, _| {
-            let content = node.cast::<Text>();
+            let content = node.cast_mut::<Text>();
+            if let Some(mut text_node) = content {
+                let mut result = text_node.content.to_owned();
+                for (pattern, replacement) in get_replacements().iter() {
+                    result = pattern
+                        .replace_all(&result, replacement.to_string())
+                        .to_string();
+                }
+                text_node.content = result;
+            }
         });
     }
+}
+
+fn get_replacements() -> &'static Box<[(Regex, &'static str)]> {
+    REPLACEMENTS.get_or_init(|| Box::new([(Regex::new(r"\+-").unwrap(), "±")]))
 }
