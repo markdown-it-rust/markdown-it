72852a7eb1aecae3a2c317738ba0013bd7a55969
Daan Wynen <black.puppydog@gmx.de>
2022-11-20 01:09:15 +0100

    Add module documentation

--- src/plugins/extra/typographer.rs
+++ src/plugins/extra/typographer.rs
@@ -1,4 +1,35 @@
 //! Common textual replacements for dashes, ©, ™, …
+//!
+//! **Note:** Since this plugin is most useful with smart-quotes, which is not
+//! currently implemented, this plugin is _not_ enabled by default when using
+//! `plugins::extra::add`. You will have to enable it separately:
+//!
+//! ```rust
+//! let md = &mut markdown_it::MarkdownIt::new();
+//! markdown_it::plugins::cmark::add(md);
+//! markdown_it::plugins::extra::add(md);
+//! markdown_it::plugins::extra::typographer::add(md);
+//!
+//! let html = md.parse("Hello world!.... This is the Right Way(TM) to markdown!!!!!").render();
+//! assert_eq!(html.trim(), r#"<p>Hello world!.. This is the Right Way™ to markdown!!!</p>"#);
+//! ```
+//! In summary, these are the replacements that will be made when using this:
+//!
+//! ## Typography
+//!
+//! - Repeated dots (`...`) to ellipsis (`…`)
+//!   except `?...` and `!...` which become `?..` and `!..` respectively
+//! - `+-` to `±`
+//! - Don't repeat `?` and `!` more than 3 times: `???`
+//! - De-duplicate commas
+//! - em and en dashes: `--` to `–` and `---` to `—`
+//!
+//! ## Common symbols (case insensitive)
+//!
+//! - Copyright: `(c)` to `©`
+//! - Reserved: `(r)` to `®`
+//! - Trademark: `(tm)` to `™`
+
 use crate::parser::core::CoreRule;
 use crate::parser::inline::Text;
 use crate::{MarkdownIt, Node};
