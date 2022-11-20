1662405179d03f3c80a38a55a5b3d9f285fbd5f7
Daan Wynen <black.puppydog@gmx.de>
2022-11-20 02:16:31 +0100

    em and en dashes
    # Conflicts:
    #	src/plugins/extra/typographer.rs

--- src/plugins/extra/typographer.rs
+++ src/plugins/extra/typographer.rs
@@ -20,9 +20,24 @@ impl CoreRule for TypographerRule {
             if let Some(mut text_node) = content {
                 let mut result = text_node.content.to_owned();
                 for (pattern, replacement) in get_replacements().iter() {
-                    result = pattern
-                        .replace_all(&result, replacement.to_string())
-                        .to_string();
+                    // This is a bit unfortunate, but since we can't use
+                    // look-ahead and look-behind patterns in the dash
+                    // replacements, the preceding and following characters (pre
+                    // and post in the patterns) become part of the match.
+                    // So a string like "bla-- --foo" would create two
+                    // *overlapping* matches, "a-- " and " --f". But replace_all
+                    // only replaces non-overlapping matches. So we can't do
+                    // this in one single replacement.
+                    // My only consolation here is that this won't happen very
+                    // often in practice, and in any case it's probably good to
+                    // ask whether the patterns match before attempting any
+                    // replacement, since that's supposed to be the cheaper
+                    // operation.
+                    while pattern.is_match(&result) {
+                        result = pattern
+                            .replace_all(&result, replacement.to_string())
+                            .to_string();
+                    }
                 }
                 text_node.content = result;
             }
@@ -38,6 +53,20 @@ fn get_replacements() -> &'static Box<[(Regex, &'static str)]> {
             (Regex::new(r"([?!])…").unwrap(), "$1.."),
             (Regex::new(r"([?!]){4,}").unwrap(), "$1$1$1"),
             (Regex::new(r",{2,}").unwrap(), ","),
+            // These look a little different from the JS implementation because the
+            // regex crate doesn't support look-behind and look-ahead patterns
+            (
+                Regex::new(r"(?m)(?P<pre>^|[^-])(?P<dash>---)(?P<post>[^-]|$)").unwrap(),
+                "$pre\u{2014}$post",
+            ),
+            (
+                Regex::new(r"(?m)(?P<pre>^|\s)(?P<dash>--)(?P<post>\s|$)").unwrap(),
+                "$pre\u{2013}$post",
+            ),
+            (
+                Regex::new(r"(?m)(?P<pre>^|[^-\s])(?P<dash>--)(?P<post>[^-\s]|$)").unwrap(),
+                "$pre\u{2013}$post",
+            ),
         ])
     })
 }
