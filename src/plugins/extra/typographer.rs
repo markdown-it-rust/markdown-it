b68b3af09335b096d54645128b7b98515eef6311
Daan Wynen <black.puppydog@gmx.de>
2022-11-20 01:52:34 +0100

    Replace ©, ®, ™
    This one turned out to be super difficult because we don't want to be
    making these replacements inside linkified URLs.
    Luckily, @rlidwka had the preceding commit ready to prevent that from
    happening.
    
    This fixes the last tests from the markdown-it fixture.

--- src/plugins/extra/typographer.rs
+++ src/plugins/extra/typographer.rs
@@ -7,39 +7,60 @@ use once_cell::sync::OnceCell;
 use regex::Regex;
 
 static REPLACEMENTS: OnceCell<Box<[(Regex, &'static str)]>> = OnceCell::new();
+static SCOPED_RE: OnceCell<Regex> = OnceCell::new();
+static RARE_RE: OnceCell<Regex> = OnceCell::new();
+
+fn replace_abbreviation(input: &str) -> &'static str {
+    match input.to_lowercase().as_str() {
+        "(c)" => "©",
+        "(r)" => "®",
+        "(tm)" => "™",
+        _ => unreachable!("Got invalid abbreviation '{}'", input),
+    }
+}
 
 pub fn add(md: &mut MarkdownIt) {
     md.add_rule::<TypographerRule>();
 }
 
 pub struct TypographerRule;
+
 impl CoreRule for TypographerRule {
     fn run(root: &mut Node, _: &MarkdownIt) {
         root.walk_mut(|node, _| {
-            let content = node.cast_mut::<Text>();
-            if let Some(mut text_node) = content {
-                let mut result = text_node.content.to_owned();
-                for (pattern, replacement) in get_replacements().iter() {
-                    // This is a bit unfortunate, but since we can't use
-                    // look-ahead and look-behind patterns in the dash
-                    // replacements, the preceding and following characters (pre
-                    // and post in the patterns) become part of the match.
-                    // So a string like "bla-- --foo" would create two
-                    // *overlapping* matches, "a-- " and " --f". But replace_all
-                    // only replaces non-overlapping matches. So we can't do
-                    // this in one single replacement.
-                    // My only consolation here is that this won't happen very
-                    // often in practice, and in any case it's probably good to
-                    // ask whether the patterns match before attempting any
-                    // replacement, since that's supposed to be the cheaper
-                    // operation.
-                    while pattern.is_match(&result) {
-                        result = pattern
-                            .replace_all(&result, replacement.to_string())
-                            .to_string();
-                    }
+            if let Some(mut text_node) = node.cast_mut::<Text>() {
+                let scoped_re = get_scoped_re();
+                if scoped_re.is_match(&text_node.content) {
+                    text_node.content = scoped_re
+                        .replace_all(&text_node.content, |caps: &regex::Captures| {
+                            replace_abbreviation(caps.get(0).unwrap().as_str())
+                        })
+                        .to_string();
                 }
-                text_node.content = result;
+                if get_rare_re().is_match(&text_node.content) {
+                    let mut result = text_node.content.to_owned();
+                    for (pattern, replacement) in get_replacements().iter() {
+                        // This is a bit unfortunate, but since we can't use
+                        // look-ahead and look-behind patterns in the dash
+                        // replacements, the preceding and following characters (pre
+                        // and post in the patterns) become part of the match.
+                        // So a string like "bla-- --foo" would create two
+                        // *overlapping* matches, "a-- " and " --f". But replace_all
+                        // only replaces non-overlapping matches. So we can't do
+                        // this in one single replacement.
+                        // My only consolation here is that this won't happen very
+                        // often in practice, and in any case it's probably good to
+                        // ask whether the patterns match before attempting any
+                        // replacement, since that's supposed to be the cheaper
+                        // operation.
+                        while pattern.is_match(&result) {
+                            result = pattern
+                                .replace_all(&result, replacement.to_string())
+                                .to_string();
+                        }
+                    }
+                    text_node.content = result;
+                }
             }
         });
     }
@@ -69,4 +90,12 @@ fn get_replacements() -> &'static Box<[(Regex, &'static str)]> {
             ),
         ])
     })
+}
+
+fn get_scoped_re() -> &'static Regex {
+    SCOPED_RE.get_or_init(|| Regex::new(r"(?i)\((c|tm|r)\)").unwrap())
+}
+
+fn get_rare_re() -> &'static Regex {
+    RARE_RE.get_or_init(|| Regex::new(r"\+-|\.\.|\?\?\?\?|!!!!|,,|--").unwrap())
 }
